fa33747a5a6db167f39028a2af258e759d05b623
aanchaljha07 <84564516+aanchaljha07@users.noreply.github.com>
2021-09-23 17:19:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+'''roots of the quadratic equation'''
+
+
+a = int(input('write the value of a:  '))
+b = int(input('write the value of b: '))
+c = int(input('write the value of c: '))
+d = b**2- 4*a*c
+x = (-b-d**0.5)/(2*a)
+x1 =(-b+d**0.5)/(2*a)
+print(' roots are : ', x, x1)
+
+
